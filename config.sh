6bc73c6271b32e55855f2f128661c31bd101d84b
SaidTec <saidtechisp@gmail.com>
2025-08-01 11:05:15 +0300

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Configuration variables
+DOMAIN="yourdomain.com"
+CLOUDFLARE_API_KEY="your_cloudflare_api_key"
+CLOUDFLARE_EMAIL="your_cloudflare_email"
+ADMIN_EMAIL="admin@yourdomain.com"
+TELEGRAM_BOT_TOKEN="your_telegram_bot_token"
+TELEGRAM_CHAT_ID="your_telegram_chat_id"
+GITHUB_REPO="joshuasaid/saidtech-vpn"
